822425367219a5a3f3e5d90e2fcbdc7efa023412
Michael Pascuzzi <gatorpazz@gmail.com>
2015-05-17 05:53:23 -0400

    Did lots of stuff, I know, not descriptive, sorry

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@
 	var deleteTask = document.querySelector('ul.todo-list');
 	var deleteTaskButtons = document.querySelectorAll('button.destroy');
 	var editTask = document.querySelectorAll('li');
+	var templateContent = document.querySelector('template').content;
 
 	//deleteTask.addEventListener('mouseover', function deleteTaskController(event){
 	//	console.log("This deletes stuff");
@@ -15,23 +16,30 @@
 		if ( event.keyCode === 13){
 			if ( newTodoInput.value !== '' ){
 				var newTask = todos.addTaskToList(newTodoInput.value.trim(), todos.taskList);
+				var clone = templateContent.cloneNode(true);
+				deleteTask.appendChild(clone);
 				newTodoInput.value = '';
+				editingTasks();
 			}
 		}
 	}); // END addEventListener(addTodoController)
-
+	
 	_.forEach(deleteTaskButtons, function(element, index, deleteTaskButtons){
 		element.addEventListener('click', function removeLi(){
 			var deleteTaskButtons = document.querySelectorAll('button.destroy');;
 			var editTask = document.querySelectorAll('li');
 			editTask[index].parentNode.removeChild(editTask[index]);
+			todos.deleteTask()
 		});
 	});
 
-	_.forEach(editTask, function(element, index, editTask){
-		element.addEventListener('dblclick', function(){
-			console.log("Edit this shit!");
+	function editingTasks(){
+		var editTask = document.querySelectorAll('li');
+		_.forEach(editTask, function(element, index, editTask){
+			element.addEventListener('dblclick', function(){
+				console.log("Edit this shit!");
+			});
 		});
-	});
+	}
 
 })(window);
